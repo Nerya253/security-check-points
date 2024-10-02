ecc41e09f4e67dec452bfad348581b7f9a6e2d89
Nerya253 <neryalevi253@gmail.com>
2024-10-02 12:31:24 +0300

    עדכון וסידור כל הקודים
    שיפור css וכפתורי מעבר בין הדפים
    תיקונים בnode 
    שיפור html והוספת הכפתורים

--- node.js
+++ node.js
@@ -1,63 +1,65 @@
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const path = require("path"); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3333;
 
-app.listen(port, () => {
-    console.log(`Live server! \nhttp://localhost:${port}`);
-});
+
 
 //מסלול כתובת ראשית
 app.get('/', (req, res) => { 
-    res.sendFile(path.join(__dirname, '/public/select.html'));
+    res.sendFile(path.join(__dirname, '/public/manager.html'));
 });
 
 //נקודות התחלה
 const points = [
-    { location: "כניסה"},
-    { location: "קומה 1"},
-    { location: "קומה 2"}
+    { id: 1, location: "כניסה"},
+    { id: 2, location: "קומה 1"},
+    { id: 3, location: "קומה 2"}
 ];
 
-app.get('/points-list', (req, res) => {
+// קריאת כל הנקודות (get)
+app.get('/points', (req, res) => {
     res.status(200).json(points);
 });
 
 //יצירת נקודה חדשה (POST)
 app.post('/points', (req, res) => {
     let point = {};
-    point.location =req.body.location;
+    point.id = req.body.idx;          
+    point.location = req.body.location;
+
     points.push(point);
 
     res.status(200).json("ok");
 });
 
 //עדכון נקודה (PATCH)
-app.patch('/points'), (req, res) => {
-    let idx = req.body.idx;
+app.patch('/points/:idx', (req, res) => {
+    let idx = req.params.idx;  // קבלת האינדקס מכתובת ה-URL
     let point = {};
     point.location = req.body.location;
     points[idx] = point;
 
     res.status(200).json("ok");
-}
-
-// קריאת כל הנקודות (get)
-app.get('/points'), (req, res) => {
-    res.status(200).json(points);
-}
+});
 
 //מחיקת נקודה (DELETE)
-app.delete('/points'), (req, res) => {
+app.delete('/points/:idx'), (req, res) => {
     let idx = req.query.id;
     points.splice(idx, 1);
 
     res.status(200).json(points);
 }
+
+
+app.listen(port, () => {
+    console.log(`Live server! \nhttp://localhost:${port}`);
+});
+
